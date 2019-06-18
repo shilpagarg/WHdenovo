22a58777da2a6085d1a6ac45a56186898bbcae8f
EC2 Default User <ec2-user@ip-172-31-43-237.us-east-2.compute.internal>
2019-06-18 19:05:52 +0000

    not required.

--- misc/difference.py
+++
@@ -1,8 +0,0 @@
-import sys
-mainset = set(open(sys.argv[1],'r').readlines())
-exc = set(open(sys.argv[2], 'r').readlines())
-diff = mainset.difference(exc)
-o = open(sys.argv[3], 'w')
-for i in diff:
-    o.write(i)
-o.close()
