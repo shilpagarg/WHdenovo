bcae03a15d1854b54b999a609a04db985fb8c597
Yichen Wang <wangych0428@gmail.com>
2019-06-19 16:54:14 +0000

    improved README.md

---
+++ whdenovo/get_unpartitioned.py
@@ -0,0 +1,21 @@
+import sys
+#from Bio import SeqIO
+
+partitioned_reads = set()
+total_reads = set()
+with open(sys.argv[1], 'r') as f:
+	for line in f:
+		var = line.rstrip()
+		partitioned_reads.add(str(var))
+
+
+with open(sys.argv[2], 'r') as f:
+	for line in f:
+		if line[0] == ">":
+			total_reads.add(line[1:].rstrip())
+unpartitioned_reads = total_reads - partitioned_reads
+print(len(unpartitioned_reads))
+
+with open(sys.argv[1]+'.unpartitioned', 'w') as f:
+	for element in unpartitioned_reads:
+		f.write(element + "\n")
